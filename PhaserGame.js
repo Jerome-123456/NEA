2998fca25a874dc8bf42ea83771ea01eb34d3c33
Jerome-123456 <DAmbrose25@synergymat.uk>
2025-10-08 14:58:26 +0100

    Add new assets and menu buttons for game levels
    Added new image assets for King John, Mughals, and Vikings. Updated PhaserGame.js to preload these assets and added interactive buttons for Start, Options, Quit, and three level selections using the new images. Also fixed button positions and included additional menu functionality.

--- PhaserGame.js
+++ PhaserGame.js
@@ -17,10 +17,20 @@ function preload() {
     this.load.image('background', 'Assets/Background.png'); 
     this.load.image('background2', 'Assets/Background2.png');
     this.load.image('StartButton', 'Assets/StartButton.png');
+    this.load.image('OptionsButton', 'Assets/OptionsButton.png');
+    this.load.image('QuitButton', 'Assets/QuitButton.png');
+    this.load.image('Mughals', 'Assets/Mughals.png');
+    this.load.imag('Vikings', 'Assets/Vikings.png');
+    this.load.image('King John', 'Assets/KingJohn.png');
 }
 function create() { 
     backgroud = this.add.image(400, 300, 'background'); 
-    StartButton = this.add.image(400, 300, 'StartButton').setInteractive().on('pointerdown', () => startGame());
+    StartButton = this.add.image(200, 500, 'StartButton').setInteractive().on('pointerdown', () => startGame());
+    OptionsButton = this.add.image(400, 500, 'StartButton').setInteractive().on('pointerdown', () => startGame());
+    QuitButton = this.add.image(600, 500, 'StartButton').setInteractive().on('pointerdown', () => startGame());
+    Level1 = this.add.image(200, 300, 'Mughals').setInteractive().on('pointerdown', () => startGame());
+    Level2 = this.add.image(400, 300, 'Vikings').setInteractive().on('pointerdown', () => startGame());
+    Level3 = this.add.image(600, 300, 'King John').setInteractive().on('pointerdown', () => startGame());
 }
 function update() {}
 
